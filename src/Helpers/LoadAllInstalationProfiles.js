483d89e51c844d558f53b94c8e2abdf4a37c20cc
Kaio Cezar M. G. Araujo <kaio.araujo@gmail.com>
2024-09-23 19:15:16 -0300

    LoadAllInstalationProfiles

--- src/Helpers/LoadAllInstalationProfiles.js
+++ src/Helpers/LoadAllInstalationProfiles.js
@@ -1,7 +1,7 @@
 
 
 
-const LoadAllInstalationProfiles = async () => {
+const LoadAllInstalationProfiles = () => {
 
     const INSTALLATION_PROFILES = {
         "dev-minimal-localfs"     : require("../InstallationProfiles/dev-localfs/minimal.install.json"),
@@ -10,8 +10,8 @@ const LoadAllInstalationProfiles = async () => {
         "standard-github-release" : require("../InstallationProfiles/github-release/standard.install.json"),
         "minimal-github-repo"     : require("../InstallationProfiles/github-repo/minimal.install.json"),
         "standard-github-repo"    : require("../InstallationProfiles/github-repo/standard.install.json"),
-        "minimal-google-drive"    : require("../InstallationProfiles/google-drive-release/minimal.install.json"),
-        "standard-google-drive"   : require("../InstallationProfiles/google-drive-release/standard.install.json")
+        "minimal-google-drive"    : require("../InstallationProfiles/google-drive/minimal.install.json"),
+        "standard-google-drive"   : require("../InstallationProfiles/google-drive/standard.install.json")
     }
 
     return INSTALLATION_PROFILES
