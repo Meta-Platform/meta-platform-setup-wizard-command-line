66b39a259fe22c6562ca7259c33d45708a490fe9
Kaio Cezar <kadisk.shark@gmail.com>
2024-10-15 20:22:59 -0300

    rafactor

--- src/Commands/Install.command.js
+++ src/Commands/Install.command.js
@@ -1,23 +1,22 @@
 const path = require("path")
 
 const SetupCLIScriptLoader = require("meta-platform-cli-script-loader-library/SetupCLIScriptLoader")
-
-const Installer = require("../Helpers/Installer")
-
-const WIZARD_CONFIGS = require("../Configs/app-params-dev.json")
+const APP_PARAMS = require("../Configs/app-params-dev.json")
 const NPM_DEPENDENCIES =  require("../Configs/npm-dependencies.json")
 const META_PLATFORM_DEPENDENCIES = require("../Configs/meta-platform-dependencies.json")
+
+const Installer = require("../Helpers/Installer")
 
 const InstallCommand = async ({ profile, installationPath }) => {   
 
     const LoaderScript = await SetupCLIScriptLoader( {
-        npmDependenciesDirname   : WIZARD_CONFIGS.NPM_DEPENDENCIES_DIRNAME,
+        npmDependenciesDirname   : APP_PARAMS.NPM_DEPENDENCIES_DIRNAME,
         npmDependencies          : NPM_DEPENDENCIES,
         metaPlatformDependencies : META_PLATFORM_DEPENDENCIES,
-        sourceType               : WIZARD_CONFIGS.MINIMUM_REPO_SOURCE_TYPE,
-        repoPath                 : WIZARD_CONFIGS.MINIMUM_REPO_PATH,
-        repoNamespace            : WIZARD_CONFIGS.MINIMUM_REPO_NAMESPACE,
-        fileId                   : WIZARD_CONFIGS.MINIMUM_REPO_FILE_ID
+        sourceType               : APP_PARAMS.MINIMUM_REPO_SOURCE_TYPE,
+        repoPath                 : APP_PARAMS.MINIMUM_REPO_PATH,
+        repoNamespace            : APP_PARAMS.MINIMUM_REPO_NAMESPACE,
+        fileId                   : APP_PARAMS.MINIMUM_REPO_FILE_ID
     })
 
     const absoluteInstallationPath = installationPath && path.resolve(process.cwd(), installationPath)
