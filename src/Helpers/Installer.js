5638e012f019ea977662cbb7e71b12bb11d4a0b3
Kaio Cezar <kadisk.shark@gmail.com>
2024-10-19 02:46:29 -0300

    ajuste config

--- src/Helpers/Installer.js
+++ src/Helpers/Installer.js
@@ -17,7 +17,7 @@ const Installer = async ({
     const installationProfiles = LoadAllInstalationProfiles()
     
     const loggerEmitter = new EventEmitter()
-	loggerEmitter.on("log", (dataLog) => PrintDataLog(dataLog, "setup-wizard - Installer"))
+	loggerEmitter.on("log", (dataLog) => PrintDataLog(dataLog, "Installer"))
 
     try{
         await InstallEcosystemByProfile({
--- src/Helpers/Updater.js
+++ src/Helpers/Updater.js
@@ -17,7 +17,7 @@ const Updater = async ({
     const installationProfiles = LoadAllInstalationProfiles()
     
     const loggerEmitter = new EventEmitter()
-	loggerEmitter.on("log", (dataLog) => PrintDataLog(dataLog, "setup-wizard - Updater"))
+	loggerEmitter.on("log", (dataLog) => PrintDataLog(dataLog, "Updater"))
 
     try{
         await UpdateEcosystemByProfile({
