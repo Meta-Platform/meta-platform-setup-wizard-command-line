13b345b8b777d6689b3af5025dc3679067769d53
Kaio Cezar <kadisk.shark@gmail.com>
2024-10-25 00:05:42 -0300

    LoadAllInstalationProfiles

--- src/Helpers/LoadAllInstalationProfiles.js
+++ src/Helpers/LoadAllInstalationProfiles.js
@@ -1,15 +1,15 @@
 const LoadAllInstalationProfiles = () => {
 
     const INSTALLATION_PROFILES = {
-        "dev-minimal-localfs"     : require("../../installation-profiles/dev-localfs/minimal.install.json"),
-        "dev-standard-localfs"    : require("../../installation-profiles/dev-localfs/standard.install.json"),
-        "dev-full-localfs"        : require("../../installation-profiles/dev-localfs/full.install.json"),
-        "minimal-github-release"  : require("../../installation-profiles/github-release/minimal.install.json"),
-        "standard-github-release" : require("../../installation-profiles/github-release/standard.install.json"),
-        "minimal-github-repo"     : require("../../installation-profiles/github-repo/minimal.install.json"),
-        "standard-github-repo"    : require("../../installation-profiles/github-repo/standard.install.json"),
-        "minimal-google-drive"    : require("../../installation-profiles/google-drive/minimal.install.json"),
-        "standard-google-drive"   : require("../../installation-profiles/google-drive/standard.install.json")
+        "dev-localfs-full"        : require("../../installation-profiles/dev-localfs-full.install.json"),
+        "dev-localfs-minimal"     : require("../../installation-profiles/dev-localfs-minimal.install.json"),
+        "dev-localfs-standard"    : require("../../installation-profiles/dev-localfs-standard.install.json"),
+        "github-release-minimal"  : require("../../installation-profiles/github-release-minimal.install.json"),
+        "github-release-standard" : require("../../installation-profiles/github-release-standard.install.json"),
+        "github-repo-minimal"     : require("../../installation-profiles/github-repo-minimal.install.json"),
+        "github-repo-standard"    : require("../../installation-profiles/github-repo-standard.install.json"),
+        "google-drive-minimal"    : require("../../installation-profiles/google-drive-minimal.install.json"),
+        "google-drive-standard"   : require("../../installation-profiles/google-drive-standard.install.json")
     }
 
     return INSTALLATION_PROFILES
