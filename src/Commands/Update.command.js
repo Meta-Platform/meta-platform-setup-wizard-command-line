dcbcd530e9dace729174dcca8ffefac00d4bac99
Kaio Cezar <kadisk.shark@gmail.com>
2025-01-18 22:22:21 -0300

    correção minimum repo

--- src/Commands/Update.command.js
+++ src/Commands/Update.command.js
@@ -16,6 +16,8 @@ const UpdateCommand = async ({ profile, installationPath }) => {
         sourceType               : APP_PARAMS.MINIMUM_REPO_SOURCE_TYPE,
         repoPath                 : APP_PARAMS.MINIMUM_REPO_PATH,
         repoNamespace            : APP_PARAMS.MINIMUM_REPO_NAMESPACE,
+        repositoryOwner          : APP_PARAMS.MINIMUM_REPO_GITHUB_OWNER,
+        repositoryName           : APP_PARAMS.MINIMUM_REPO_GITHUB_NAME,
         fileId                   : APP_PARAMS.MINIMUM_REPO_FILE_ID
     })
 
